9f873ffa980e71c4c9bd6d08260801a30983ce8c
Dustin Specker <dustinspecker@dustinspecker.com>
2015-11-30 06:03:25 -0600

    refactor(test): use ES2015

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 'use strict';
-var test = require('ava');
-var getSetProps = require('./get-set-props');
+import getSetProps from './get-set-props';
+import test from 'ava';
 
-test(function (t) {
+test(t => {
 	t.assert(Object.keys(getSetProps).length > 0);
 	t.assert(getSetProps.hasOwnProperty('Array'));
 	t.assert(getSetProps.hasOwnProperty('Number'));
