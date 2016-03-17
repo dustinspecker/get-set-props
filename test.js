4537dd6d035a33d8973ce03013043869d759d2d0
Dustin Specker <DustinSpecker@DustinSpecker.com>
2016-03-16 19:40:29 -0500

    chore(deps): update AVA
    Closes #16

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ import getSetProps from './get-set-props';
 import test from 'ava';
 
 test(t => {
-	t.assert(Object.keys(getSetProps).length > 0);
-	t.assert(getSetProps.hasOwnProperty('Array'));
-	t.assert(getSetProps.hasOwnProperty('Number'));
-	t.assert(getSetProps.Error.indexOf('stack') !== -1);
+	t.ok(Object.keys(getSetProps).length > 0);
+	t.ok(getSetProps.hasOwnProperty('Array'));
+	t.ok(getSetProps.hasOwnProperty('Number'));
+	t.ok(getSetProps.Error.indexOf('stack') !== -1);
 });
