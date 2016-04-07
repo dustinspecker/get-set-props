d4a880ce1f83328ae58b825bc2f68046e45cbf4d
Dustin Specker <DustinSpecker@DustinSpecker.com>
2016-04-07 16:15:32 -0500

    chore(deps): update AVA

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ import getSetProps from './get-set-props';
 import test from 'ava';
 
 test(t => {
-	t.ok(Object.keys(getSetProps).length > 0);
-	t.ok(getSetProps.hasOwnProperty('Array'));
-	t.ok(getSetProps.hasOwnProperty('Number'));
-	t.ok(getSetProps.Error.indexOf('stack') !== -1);
+	t.truthy(Object.keys(getSetProps).length > 0);
+	t.truthy(getSetProps.hasOwnProperty('Array'));
+	t.truthy(getSetProps.hasOwnProperty('Number'));
+	t.truthy(getSetProps.Error.indexOf('stack') !== -1);
 });
